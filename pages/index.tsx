8903d98c190f31aaf1163a65f348116af89b5421
jeongseungmin <jsm8109jsm@gmail.com>
2023-04-05 20:14:44 +0900

    FEAT :: 빈자리 처리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,38 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import { DocumentData, collection, getDocs } from "firebase/firestore";
+import {
+  DocumentData,
+  collection,
+  query,
+  orderBy,
+  getDocs,
+} from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { fireStore } from "@/config/firebaseConfig";
 
 interface SeatType {
-  [x: string]: DocumentData;
+  [x: string]: DocumentData | string;
 }
 
 export default function Home() {
   const bucket = collection(fireStore, "students");
+  const q = query(bucket, orderBy("current_seat"));
   const [students, setStudents] = useState<SeatType[]>([]);
   useEffect(() => {
     (async () => {
       try {
-        const response = await getDocs(bucket);
-        const newData: SeatType[] = [];
+        const response = await getDocs(q);
+        console.log(response);
+        let newData: SeatType[] = [];
         response.docs.map((doc) => {
           newData.push(doc.data());
         });
+        newData = [
+          ...newData.slice(0, 12),
+          { name: "빈자리" },
+          ...newData.slice(12),
+          { name: "빈자리" },
+        ];
         setStudents(newData);
       } catch (error) {
         console.log(error);
@@ -26,7 +40,6 @@ export default function Home() {
     })();
   }, []);
 
-  console.log(students);
   return (
     <>
       <Head>
@@ -39,11 +52,13 @@ export default function Home() {
             return (
               <div key={index} className={styles.seat}>
                 <span>
-                  {`32${
-                    Number(item.id) > 9
-                      ? String(item.id)
-                      : "0" + String(item.id)
-                  }`}
+                  {item.id
+                    ? `32${
+                        Number(item.id) > 9
+                          ? String(item.id)
+                          : "0" + String(item.id)
+                      }`
+                    : ""}
                 </span>
                 <span>{String(item.name)}</span>
               </div>
