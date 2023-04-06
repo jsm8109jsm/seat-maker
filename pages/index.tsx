5c153f00d1d4156e2627c160e15680dff31de61a
jeongseungmin <jsm8109jsm@gmail.com>
2023-04-06 17:10:47 +0900

    FEAT :: Image export

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,10 @@ import {
   doc,
   updateDoc,
 } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { fireStore } from "@/config/firebaseConfig";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
 
 interface SeatType {
   [x: string]: DocumentData | string | number;
@@ -21,6 +23,24 @@ export default function Home() {
   const q = query(bucket, orderBy("current_seat"));
   const [students, setStudents] = useState<SeatType[]>([]);
   const [render, setRender] = useState(false);
+  const date = new Date();
+
+  const cardRef = useRef<HTMLDivElement>(null);
+  const onDownloadBtn = () => {
+    const card = cardRef.current;
+    domtoimage.toBlob(card as Node).then((blob) => {
+      saveAs(
+        blob,
+        `${
+          date.getMonth() + 1 > 9
+            ? date.getMonth() + 1
+            : "0" + String(date.getMonth() + 1)
+        }${
+          date.getDate() > 9 ? date.getDate() : "0" + String(date.getDate())
+        } 3-2 자리배치도.png`
+      );
+    });
+  };
 
   const setEmptySeat = (seats: SeatType[]) => {
     return (seats = [
@@ -79,8 +99,8 @@ export default function Home() {
       <Head>
         <title>3-2반 자리 뽑기 사이트!</title>
       </Head>
-      <div>
-        <h1 className={styles.title}>3-2반 자리 뽑기 사이트!</h1>
+      <div ref={cardRef} className={styles.background}>
+        <h1 className={styles.title}>3-2반 자리 배치도</h1>
         <div className={styles.seat_container}>
           {[...students].reverse().map((item, index) => {
             return (
@@ -99,9 +119,16 @@ export default function Home() {
             );
           })}
         </div>
+        <div className={styles.desk}>교탁</div>
       </div>
-      <div className={styles.desk}>교탁</div>
-      <button onClick={() => shuffleSeats()}>자리 섞기!</button>
+      <div className={styles.buttons}>
+        <button onClick={() => shuffleSeats()} className={styles.button}>
+          자리 섞기!
+        </button>
+        <button onClick={() => onDownloadBtn()} className={styles.button}>
+          사진 저장!
+        </button>
+      </div>
     </>
   );
 }
